bb3f7c4b9edfcb9984899089603c142e601db485
Romit Maharjan <romitmaharjan25@gmail.com>
2021-09-02 13:16:09 +1000

    hunting now

---
+++ Desktop/candyHunter.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import time
+
+url = "https://coingecko.com/"
+driver = webdriver.Firefox(executable_path=r'PATH_TO_geckodriver\geckodriver.exe') 
+driver.get(url)
+
+loginButton = driver.find_element_by_class_name('mr-3.text-black').click()
+
+driver.find_element_by_name("user[email]").send_keys("email")
+
+driver.find_element_by_name("user[password]").send_keys("password")
+
+driver.find_element_by_class_name("btn.btn-primary.btn-lg.btn-block").click()
+
+driver.find_element_by_class_name("candy-notification-icon").click()
+
+driver.find_element_by_class_name("btn.btn-primary.col-12.collect-candy-button").click()
+
+driver.quit()
